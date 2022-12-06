eb14d24eff29f09b3908ce64db8397e01e918af1
IAN-97-w <105089699+IAN-97-w@users.noreply.github.com>
2022-12-06 15:20:57 +0900

    member 테스트

--- MukNolJa/src/main/java/com/spring/muknolja/Test.java
+++ MukNolJa/src/main/java/com/spring/muknolja/Test.java
@@ -1,5 +1,12 @@
 package com.spring.muknolja;
 
-public class Test {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class Test {
+	@GetMapping("member.me")
+	public String member() {
+		return "member";
+	}
 }
